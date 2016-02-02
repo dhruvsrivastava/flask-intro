0629d5a8218e4acc3a4bbf26be8a80acda67f919
Dhruv <dhruv.10000@gmail.com>
2016-02-02 21:29:09 +0530

    slave branch

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # import the Flask class from the flask module
 from flask import Flask, render_template
 from flask import redirect , request , url_for
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
 
 from flask import g
 #global flask object
@@ -17,10 +19,40 @@ def home():
 @app.route('/about_me')
 def about_me():
 	return render_template('about_me.html')
-    return "Hello World!"  # return a string
 
-@app.route('/display')
-def display():
+@app.route('/404')
+def Error_404():
+    return render_template('404.html')
+
+@app.route('/users/<username>')
+def user_profile(username):
+    if username != "dhruv":
+        return redirect('/404') 
+    return "Hello %s" %username
+
+@app.route('/contest_list')
+def contest_list():
+    url=urlopen("http://clist.by/")
+    soup = BeautifulSoup(url)
+    x = []
+    y = []
+    for link in soup.find_all('div' , class_ = "contest-title"):
+        x.append(link.a["title"])
+        y.append(link.a["href"])
+        # print link.a["title"]
+        # print link.a["href"]
+    return render_template("clist.html" , cnames = x , clinks = y , item = x[0])
+
+@app.route('/submissions')
+def submissions():
+	g.db = connect_db()
+	cur = g.db.execute('select * from codes')
+	codes = [dict(id = row[0] , code = row[1]) for row in cur.fetchall()]
+	g.db.close()
+	return render_template('submissions.html' , codes = codes)
+
+@app.route('/posts')
+def posts():
     g.db = connect_db()
     cur = g.db.execute('select * from posts')
     posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
--- sql.py
+++ sql.py
@@ -7,3 +7,10 @@ with sqlite3.connect("sample.db") as connection:
 	c.execute('CREATE TABLE posts(title TEXT , description TEXT) ')
 	c.execute('INSERT INTO posts VALUES("Good" , "I\'m good")')
 	c.execute('INSERT INTO posts VALUES("Well" , "I\'m well")')
+
+	c.execute('DROP TABLE codes')
+	c.execute('CREATE TABLE codes(id INTEGER , code TEXT) ')
+	codeString = 'print "Hello world" '
+	c.execute("INSERT INTO codes VALUES (1, ?)", (codeString,))
+	codeString = 'print "Second code" '
+	c.execute("INSERT INTO codes VALUES (2, ?)", (codeString,))
